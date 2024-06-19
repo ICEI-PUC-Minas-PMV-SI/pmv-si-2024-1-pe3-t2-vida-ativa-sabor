5c616563d6cba98c5f68589f652e87981ce90d72
Isadora Cardoso <101332900+isadoraacardoso@users.noreply.github.com>
2024-06-19 11:34:37 -0300

    Teste de manipulação de dados das receitas

---
+++ Src/dbfake.js
@@ -0,0 +1,34 @@
+var db = {
+    dados:[
+        {
+            título: "Salada Tropical",
+            imagem: "salada-tropical.jpg",
+            ingredientes: "Alface Americana"
+        },
+        {
+            título: "Peixe a milanesa",
+            imagem: "salada-tropical.jpg",
+            ingredientes: "Peixe, queijo, molho de tomate"
+        },
+        {
+            título: "Bife a cavalo",
+            imagem: "salada-tropical.jpg",
+            ingredientes: "Filé de vaca, ovos"
+        },
+        {
+            título: "Tapioca",
+            imagem: "salada-tropical.jpg",
+            ingredientes: "Tapioca, ovos"
+        },
+        {
+            título: "Ovos mexidos",
+            imagem: "salada-tropical.jpg",
+            ingredientes: "Ovos, sal a gosto"
+        },
+        {
+            título: "Suco detox",
+            imagem: "salada-tropical.jpg",
+            ingredientes: "Couve, laranja, maçã, água"
+        }
+    ]
+}
