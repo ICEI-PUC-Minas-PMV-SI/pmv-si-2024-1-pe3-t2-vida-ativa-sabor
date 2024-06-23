38343bc84944a57d9c81edee23a86da0ee6136b9
Ricardooandrade <128002303+Ricardooandrade@users.noreply.github.com>
2024-06-23 11:27:08 -0300

    mudanca server

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const PORT = process.env.PORT || 5500;
 const db = new sqlite3.Database('database.db');
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.json()); // Isso substitui o bodyParser.json()
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src'));
-app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'src')));
 
 app.post('/dados', (req, res) => {
@@ -27,7 +26,6 @@ app.post('/dados', (req, res) => {
     });
 });
 
-
 app.get('/login', (req, res) => {
     res.render('login', { nome: '' }); 
 });
@@ -40,13 +38,11 @@ app.post('/login', (req, res) => {
             console.error('Erro ao verificar credenciais:', err);
             res.status(500).send('Erro ao verificar credenciais.');
         } else if (row) {
-            
             res.cookie('nome', row.NOME);
             res.cookie('id', row.ID); 
             res.cookie('adm',row.ADM);
             res.redirect('/');
         } else {
-            
             res.redirect('/login?loginError=true');
         }
     });
@@ -67,7 +63,6 @@ app.post('/cadastro', (req, res) => {
 });
 
 app.get('/usuarios', (req, res) => {
-    
     db.all('SELECT * FROM users', (err, rows) => {
         if (err) {
             console.error('Erro ao consultar usuários:', err);
@@ -78,23 +73,17 @@ app.get('/usuarios', (req, res) => {
     });
 });
 
-
 app.get('/', (req, res) => {
     const NOME = req.cookies.nome || '';
     const ID = req.cookies.id || ''; 
     res.render('index', { nome: NOME, id: ID });
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
 app.post('/editar-usuario', (req, res) => {
     const { ID, NOME, DATNAS, EMAIL, SENHA, TEL } = req.body;
 
-    
     console.log('Dados recebidos para edição:', { ID, NOME, DATNAS, EMAIL, SENHA, TEL });
 
-    
     db.run('UPDATE users SET NOME = ?, DATNAS = ?, EMAIL = ?, SENHA = ?, TEL = ? WHERE ID = ?', 
         [NOME, DATNAS, EMAIL, SENHA, TEL, ID], 
         (err) => {
@@ -109,7 +98,6 @@ app.post('/editar-usuario', (req, res) => {
     );
 });
 
-// Iniciar o servidor
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
